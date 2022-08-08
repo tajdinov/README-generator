08bedf56c65acd7bd5fcbec7586fafdd10ca39ea
Roman Tazhdynov <105038717+tajdinov@users.noreply.github.com>
2022-08-08 17:26:17 +0930

    Second commit
    updated .gitignore
    updated index.js
    (an array of questions/prompts, function to write to file

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,70 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require("./utils/generateMarkdown.js");
+// An array of questions for user input
+const questions = () => {
+    return inquirer.prompt([{
+        type: 'input',
+        name: 'title',
+        messsage: "What is the title of this project?"
+    },
+    {
+        type: 'input',
+        name: 'description',
+        messsage: "Provide a description for your project"  
+    },
+    {
+        type: 'input',
+        name: 'instalation',
+        messsage: "What are the steps required to install your project?"  
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        messsage: "What are the steps required to use your project?"  
+    },
+    {
+        type: 'input',
+        name: 'contributions',
+        messsage: "Are there any contribution guideliness that need to be followed?"  
+    },
+    {
+        type: 'input',
+        name: 'test',
+        messsage: "Are there any instructions in regards to testing?"  
+    },
+    {
+        type: 'input',
+        name: 'license',
+        messsage: 'Please select the licensing required for this application.',
+        choices: [
+            "Apache License 2.0",
+            "GNU General Public License v3.0",
+            "MIT License",
+            "None Required"
+        ]
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email."
+    },
+    {
+        type: "input",
+        name: "git",
+        message: "Please provide your GitHub Profile username."
+    },
+    ])
+};
 
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
+// Function to initialize and write to file
+const init= () => {
+    questions()
+    .then((answers) => fs.writeFileSync('README.md', generateMarkdown(answers)))
+    .then(() => console.log("You succesfully generated the README.md"))
+    .catch((err) => console.log(err))
+};
 
 // Function call to initialize app
 init();
